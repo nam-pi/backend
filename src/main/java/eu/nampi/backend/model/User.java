091012457115378becc3ded67e9081dbff0a27da
Daniel Jeller <djeller@nettek.at>
2021-01-21 18:49:25 +0000

    Add lombok

--- src/main/java/eu/nampi/backend/model/User.java
+++ src/main/java/eu/nampi/backend/model/User.java
@@ -4,6 +4,13 @@ import java.util.UUID;
 
 import org.springframework.lang.NonNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
 
   @NonNull
@@ -14,35 +21,5 @@ public class User {
 
   @NonNull
   private String email;
-
-  public User(UUID id, String username, String email) {
-    this.id = id;
-    this.username = username;
-    this.email = email;
-  }
-
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
 
 }
