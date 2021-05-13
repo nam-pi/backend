3f721089e29ab82ccf4b8dc22daf4eadd16913d7
Daniel Jeller <djeller@nettek.at>
2021-05-13 16:26:52 +0000

    Add new source repository

--- src/main/java/eu/nampi/backend/repository/SourceRepository.java
+++ src/main/java/eu/nampi/backend/repository/SourceRepository.java
@@ -1,18 +1,28 @@
 package eu.nampi.backend.repository;
 
+import static eu.nampi.backend.model.hydra.temp.AbstractHydraBuilder.VAR_COMMENT;
+import static eu.nampi.backend.model.hydra.temp.AbstractHydraBuilder.VAR_LABEL;
+import static eu.nampi.backend.model.hydra.temp.AbstractHydraBuilder.VAR_MAIN;
+import java.util.Optional;
 import java.util.UUID;
-import org.apache.jena.arq.querybuilder.ConstructBuilder;
+import java.util.function.BiFunction;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.Lang;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 import eu.nampi.backend.model.QueryParameters;
-import eu.nampi.backend.model.hydra.HydraCollectionBuilder;
-import eu.nampi.backend.model.hydra.HydraSingleBuilder;
+import eu.nampi.backend.model.hydra.temp.AbstractHydraBuilder;
+import eu.nampi.backend.model.hydra.temp.HydraCollectionBuilder;
+import eu.nampi.backend.model.hydra.temp.HydraSingleBuilder;
 import eu.nampi.backend.vocabulary.Api;
 import eu.nampi.backend.vocabulary.Core;
 import eu.nampi.backend.vocabulary.SchemaOrg;
@@ -23,32 +33,42 @@ public class SourceRepository extends AbstractHydraRepository {
 
   private static final Node VAR_SAME_AS = NodeFactory.createVariable("sameAs");
 
-  public Model findAll(QueryParameters params) {
-    HydraCollectionBuilder builder = new HydraCollectionBuilder(endpointUri("sources"), Core.source,
-        Api.sourceOrderByVar, params);
-    builder.dataWhere.addOptional(HydraCollectionBuilder.VAR_MAIN, SchemaOrg.sameAs, VAR_SAME_AS);
-    addData(builder, HydraCollectionBuilder.VAR_MAIN);
-    return construct(builder);
-  }
+  private static final BiFunction<Model, QuerySolution, RDFNode> ROW_MAPPER = (model, row) -> {
+    Resource main = row.getResource(VAR_MAIN.toString());
+    // Main
+    model.add(main, RDF.type, Core.source);
+    // Label
+    Optional.ofNullable(row.getLiteral(VAR_LABEL.toString())).map(Literal::getString)
+        .ifPresent(label -> model.add(main, RDFS.label, label));
+    // Comment
+    Optional.ofNullable(row.getLiteral(VAR_COMMENT.toString())).map(Literal::getString)
+        .ifPresent(comment -> model.add(main, RDFS.comment, comment));
+    // SameAs
+    Optional.ofNullable(row.getResource(VAR_SAME_AS.toString())).map(Resource::getURI)
+        .ifPresent(string -> model.add(main, SchemaOrg.sameAs, string));
+    return main;
+  };
 
   @Cacheable(
       key = "{#lang, #params.limit, #params.offset, #params.orderByClauses, #params.type, #params.text}")
   public String findAll(QueryParameters params, Lang lang) {
-    Model model = findAll(params);
-    return serialize(model, lang, ResourceFactory.createResource(endpointUri("sources")));
+    HydraCollectionBuilder builder = new HydraCollectionBuilder(jenaService, endpointUri("sources"),
+        Core.source, Api.sourceOrderByVar, params);
+    builder.extendedData.addOptional(VAR_MAIN, SchemaOrg.sameAs, VAR_SAME_AS);
+    return build(builder, lang);
   }
 
   @Cacheable(key = "{#lang, #id}")
   public String findOne(Lang lang, UUID id) {
     HydraSingleBuilder builder =
-        new HydraSingleBuilder(individualsUri(Core.source, id), Core.source);
-    addData(builder, HydraSingleBuilder.VAR_MAIN);
-    builder.addOptional(HydraSingleBuilder.VAR_MAIN, SchemaOrg.sameAs, VAR_SAME_AS);
-    Model model = construct(builder);
-    return serialize(model, lang, ResourceFactory.createResource(builder.iri));
+        new HydraSingleBuilder(jenaService, individualsUri(Core.source, id), Core.source);
+    builder.coreData.addOptional(VAR_MAIN, SchemaOrg.sameAs, VAR_SAME_AS);
+    return build(builder, lang);
   }
 
-  private void addData(ConstructBuilder builder, Node varMain) {
-    builder.addConstruct(varMain, SchemaOrg.sameAs, VAR_SAME_AS);
+  private String build(AbstractHydraBuilder builder, Lang lang) {
+    builder.build(ROW_MAPPER);
+    return serialize(builder.model, lang, builder.root);
   }
+
 }
