279c97035029edc99f221a2f8d6fa04d79753139
Daniel Jeller <djeller@nettek.at>
2021-09-20 23:10:42 +0200

    Add constructors to NotFoundException

--- src/main/java/eu/nampi/backend/exception/NotFoundException.java
+++ src/main/java/eu/nampi/backend/exception/NotFoundException.java
@@ -1,4 +1,12 @@
 package eu.nampi.backend.exception;
 
 public class NotFoundException extends RuntimeException {
+
+  public NotFoundException() {
+    super();
+  }
+
+  public NotFoundException(String string) {
+    super(string);
+  }
 }
